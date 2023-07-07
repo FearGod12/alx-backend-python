f8804dfe7e2cf0e78107d72a66967a45438c30bf
Onyenike Chukwudi FearGod <onyenikechukwudi@gmail.com>
2023-07-07 23:26:39 +0100

    task 100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""duck typing"""
+from typing import Sequence, Union, Any, TypeAlias
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """returns Type Any or None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
