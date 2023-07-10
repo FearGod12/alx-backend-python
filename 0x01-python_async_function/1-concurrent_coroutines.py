c6758402a5318d36010eb2c2041c8b09bbb5f1f7
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-10 23:35:16 +0000

    more fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,11 +6,12 @@ wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without
 using sort() because of concurrency."""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay"""
     res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
 
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """Spawn wait_random n times with the specified max_delay"""
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay"""
-    res = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    res = await asyncio.gather(*(task_wait_random(
+                                 max_delay) for _ in range(n)))
 
     # Sort result in ascending order
     for i in range(len(res)):
