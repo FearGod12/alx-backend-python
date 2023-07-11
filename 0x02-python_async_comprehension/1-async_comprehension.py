b20abc77c077bf136f932eb16092b9e6d40096a6
Onyenike Chukwudi FearGod <onyenikechukwudi@gmail.com>
2023-07-11 16:09:22 +0100

    task 1: fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """coroutine called async_comprehension that takes no arguments"""
 from typing import List
+import asyncio
 
 async_generator = __import__('0-async_generator').async_generator
 
@@ -8,4 +9,5 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """collect 10 random numbers using an async comprehensing over
     async_generator then return the 10 random numbers"""
-    return await [await i for i in async_generator()]
+    numbers = [i async for i in async_generator()]
+    return numbers
