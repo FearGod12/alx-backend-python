781ba30a5e60e618dd3d3482412cde0bb59a53f5
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-31 20:45:22 +0100

    fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -10,15 +10,26 @@ from client import GithubOrgClient, get_json
 class TestGithubOrgClient(unittest.TestCase):
     """tests the githuborgclient class"""
 
-    @parameterized(["google",
-                    "abc"
-                    ])
-    @patch("get_json")
-    def test_org(self, org, mock_get_json):
-        """tests if githubOrgClient.org returns the correct value"""
-        mock_get_json.return_value = "https://api.github.com/orgs/{}".\
-            format(org)
-        gitorg = GithubOrgClient()
-        result = gitorg.org()
-        mock_get_json.assert_called_once()
-        self.assertEqual(result, "https://api.github.com/orgs/{}".format(org))
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.GithubOrgClient.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test the org method of GithubOrgClient"""
+
+        # Set up the mock get_json method to return a specific value
+        mock_get_json.return_value = {"login": org_name}
+
+        # Create an instance of GithubOrgClient with the org_name
+        client = GithubOrgClient(org_name)
+
+        # Call the org method
+        result = client.org()
+
+        # Check that get_json was called exactly once with the correct argument
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+        # Check that the result is as expected
+        self.assertEqual(result, {"login": org_name})
+        
