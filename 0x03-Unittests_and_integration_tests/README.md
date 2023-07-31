ddd203cfdef10e509519ec28d4f2380880766674
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-31 14:50:15 +0100

    task 0

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,2 @@
+<h1>python unittest and integration tests</h1>
+<h3>unittest testing framework, unittest.mock, parameterized, memoized</h3>[utils.py](utils.py)
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""test module for the utils"""
+
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """tests for utils class"""
+
+    @parameterized.expand([({"a": 1}, ("a",), 1),
+                           ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                           ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self, nested_map, path, result):
+        """test the access_nested_map method"""
+        self.assertEqual(access_nested_map(nested_map, path), result)
