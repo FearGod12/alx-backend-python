23b499efaa54f4561dd0f7dc5c30e98ce6da73c0
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-31 18:39:48 +0100

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """test module for the utils"""
 
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch
@@ -42,3 +42,33 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once()
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        """Test memoization of a_property method"""
+
+        # Define the TestClass with a_method and a_property
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Patch the a_method
+        with patch.object(TestClass, "a_method") as mock_a_method:
+            # Create an instance of TestClass
+            test_class_instance = TestClass()
+
+            # Call a_property twice
+            result1 = test_class_instance.a_property()
+            result2 = test_class_instance.a_property()
+
+            # Check that a_method was called only once
+            mock_a_method.assert_called_once()
+
+            # Check that the results are equal
+            self.assertEqual(result1, result2)
