80e31f40df7dac337581a3186e9c85565f48d1b1
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-02 14:36:55 +0100

    added more comments

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""test module for the utils"""
+"""test module for the utils
+and all its methods"""
 
 from utils import access_nested_map, get_json, memoize
 import unittest
@@ -9,7 +10,8 @@ import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """tests for utils class"""
+    """tests for utils class
+    and its methods"""
 
     @parameterized.expand([({"a": 1}, ("a",), 1),
                            ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -23,12 +25,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """tests the access_nested map method
+        and its return value"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """tests the get_json method"""
+    """tests the get_json method
+    and its return value"""
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -36,7 +41,8 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
-        """tests get_+json"""
+        """tests get_+json
+        and its return value"""
         response = mock_get.return_value
         response.json.return_value = test_payload
         result = get_json(test_url)
@@ -45,21 +51,26 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """tests memoization"""
+    """tests memoization
+    for testing purpose"""
 
     def test_memoize(self):
-        """Test memoization of a_property method"""
+        """Test memoization of a_property method
+        to test if it caches"""
 
         # Define the TestClass with a_method and a_property
         class TestClass:
-            """class to be used for testing"""
+            """class to be used for testing
+            purpose only"""
             def a_method(self):
-                """memoized method"""
+                """memoized method
+                to be cached"""
                 return 42
 
             @memoize
             def a_property(self):
-                """calls a_method"""
+                """calls a_method
+                and see if it caches"""
                 return self.a_method()
 
         # Patch the a_method
